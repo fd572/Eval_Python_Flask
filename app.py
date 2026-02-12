fc835ae1bf3de512dd48f2548d59f0591a18f2ae
Franck Doyen <franck.doyen57@gmail.com>
2026-02-12 20:34:23 +0100

    feat(eval_flask): testing database

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, redirect, render_template, request, flash, url_for
+from flask import Flask, redirect, render_template, request, flash, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "onePiece"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///evenement.db'
 
 db = SQLAlchemy(app)
 
-class Database(db.Model):
+class Evenement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titre = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(50), nullable=False)
@@ -20,31 +20,37 @@ class Database(db.Model):
 
 
     def __repr__(self):
-       return f"Database {self.titre}, {self.type}, {self.date}, {self.lieu}, {self.description}, {self.date_creation}"
+       return f"Evenement {self.titre}, {self.type}, {self.date}, {self.lieu}, {self.description}, {self.date_creation}"
 
 with app.app_context():
     db.create_all()
 
 @app.route("/index",methods=["GET"])
-def accueil():
-    database_entry = Database.query.filter(Database.date >= datetime.now()).order_by(Database.date.asc()).limit(5).all()
+def index():
+    Evenement_entry = Evenement.query.filter(Evenement.date >= datetime.now()).order_by(Evenement.date.asc()).limit(5).all()
     clean_db = []
-    for d in database_entry:
+    for e in Evenement_entry:
         clean_db.append({
-        "id": d.id,
-        "titre": d.titre,
-        "type": d.type,
-        "date": d.date,
-        "lieu": d.lieu,
-        "description": d.description,
-        "date_creation": d.date_creation
+        "id": e.id,
+        "titre": e.titre,
+        "type": e.type,
+        "date": e.date,
+        "lieu": e.lieu,
+        "description": e.description,
+        "date_creation": e.date_creation
         })
 
     return jsonify(
     {
-      "database": clean_db
+      "evenements": clean_db
     }
   ), 200
+
+@app.route("/",methods=["GET"])
+def accueil():
+    evenements = Evenement.query.all()
+    return render_template("index.html", evenements=evenements)
+
 
 @app.route("/",methods=["POST"])
 def accueil_remove():
